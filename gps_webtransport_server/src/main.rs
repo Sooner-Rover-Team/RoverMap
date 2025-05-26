ceaac1ae5f6f9926d4db4dd7170d747064205638
logbasem <loganmichellemurphy@gmail.com>
2025-05-26 13:42:26 -0500

    feat: added updating coord to confirm that marker moves correctly

--- gps_webtransport_server/src/main.rs
+++ gps_webtransport_server/src/main.rs
@@ -1,6 +1,10 @@
 use futures_util::{SinkExt, StreamExt};
-use tokio::net::TcpListener;
+use tokio::{
+    net::TcpListener,
+    time::{interval, Duration},
+};
 use tokio_tungstenite::accept_async;
+use tungstenite::Message;
 
 #[tokio::main]
 async fn main() {
@@ -11,12 +15,26 @@ async fn main() {
         tokio::spawn(async move {
             let ws_stream = accept_async(stream).await.unwrap();
             let (mut write, _) = ws_stream.split();
-            //38.4375, -110.8125
-            let gps_data = r#"{"lat": 38.4375, "lon": -110.8125}"#;
-            write
-                .send(tungstenite::Message::Text(gps_data.into()))
-                .await
-                .unwrap();
+
+            let mut interval = interval(Duration::from_secs(1));
+            let mut toggle = true;
+
+            loop {
+                interval.tick().await;
+
+                let (lat, lon) = if toggle {
+                    (38.4375, -110.8125)
+                } else {
+                    (38.4380, -110.8130) // Slightly different location
+                };
+                toggle = !toggle;
+
+                let gps_data = format!(r#"{{"lat": {}, "lon": {}}}"#, lat, lon);
+                if let Err(e) = write.send(Message::Text(gps_data.into())).await {
+                    eprintln!("❌ WebSocket send error: {}", e);
+                    break;
+                }
+            }
         });
     }
 }
