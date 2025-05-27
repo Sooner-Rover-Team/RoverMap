40fc685c9b38e88e403b68899efe45eaad2ebb9e
logbasem <loganmichellemurphy@gmail.com>
2025-05-27 11:14:49 -0600

    chore: got rid of some unnecessary comments

--- frontend/map.js
+++ frontend/map.js
@@ -22,16 +22,3 @@ tileLayer.on('tileerror', function (e) {
 tileLayer.addTo(map);
 
 console.log(new URL('/utah-tiles-2/17/25188/50355.png', window.location.href).href);
-
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '© OpenStreetMap contributors'
-// }).addTo(map);
-
-// Rover marker polling
-// TODO: Implement this. Commented out for now because the output is annoying
-let roverMarker = new L.marker([0, 0]).addTo(map);
-/*setInterval(() => {
-    fetch("/roverCoords").then(res => res.json()).then(data => {
-        roverMarker.setLatLng(data)
-    });
-}, 1000);*/
